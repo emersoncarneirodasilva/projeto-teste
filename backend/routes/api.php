886998026205414ac221254fa0af47abf23ed66c
Marlon Note Proprio <marlon.aelson.gomes@gmail.com>
2024-08-24 21:25:55 -0300

    ajustado rota do back

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -22,8 +22,8 @@ Route::get('/form/{id}',    [ FormController::class, 'show' ]); //mostra o regis
 Route::put('/form/{id}',  [ FormController::class, 'update' ]); //atualiza os dados do registro do ID passado como parâmetro
 Route::delete('/form/{id}', [ FormController::class, 'destroy' ]); //deleta o registro do ID passado como parâmetro
 
-Route::get('/to-do-list',         [ ToDoListController::class, 'index' ]); //retorna todos os registros cadastrado no banco de dados
-Route::post('/to-do-list',        [ ToDoListController::class, 'store' ]); //armazena o cadastro no banco de dados
-Route::get('/to-do-list/{id}',    [ ToDoListController::class, 'show' ]); //mostra o registro do ID passado como parâmetro
-Route::put('/to-do-list/{id}',  [ ToDoListController::class, 'update' ]); //atualiza os dados do registro do ID passado como parâmetro
-Route::delete('/to-do-list/{id}', [ ToDoListController::class, 'destroy' ]); //deleta o registro do ID passado como parâmetro
+Route::get('/todo',         [ ToDoListController::class, 'index' ]); //retorna todos os registros cadastrado no banco de dados
+Route::post('/todo',        [ ToDoListController::class, 'store' ]); //armazena o cadastro no banco de dados
+Route::get('/todo/{id}',    [ ToDoListController::class, 'show' ]); //mostra o registro do ID passado como parâmetro
+Route::put('/todo/{id}',  [ ToDoListController::class, 'update' ]); //atualiza os dados do registro do ID passado como parâmetro
+Route::delete('/todo/{id}', [ ToDoListController::class, 'destroy' ]); //deleta o registro do ID passado como parâmetro
